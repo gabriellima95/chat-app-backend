bfae5274c29019f98292a15b4fd621c8b4f2d872
Gabriel Lima <gabrielkvalknt@gmail.com>
2023-01-22 02:25:27 -0300

    Publish CreateGenericMessage endpoint

--- cmd/http/server.go
+++ cmd/http/server.go
@@ -31,6 +31,7 @@ func handleRequests(userController controllers.UserController, chatController co
 	myRouter.HandleFunc("/{user_id}/generic_chats", chatController.ListGenericChats).Methods("GET")
 	myRouter.HandleFunc("/{chat_id}/messages", messageController.ListMessages).Methods("GET")
 	myRouter.HandleFunc("/messages", messageController.CreateMessage).Methods("POST")
+	myRouter.HandleFunc("/generic_messages", messageController.CreateGenericMessage).Methods("POST")
 	myRouter.HandleFunc("/data", dataController.Populate).Methods("POST")
 	myRouter.HandleFunc("/data", dataController.Clear).Methods("DELETE")
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
